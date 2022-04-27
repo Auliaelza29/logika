023ba52675c99c651a6a4efdd54dfd8633380dcf
Aulia Elza Marchella <88173311+Auliaelza29@users.noreply.github.com>
2022-04-28 06:39:05 +0700

    Add files via upload

---
+++ app.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int num1, num2, num3, greatest;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Minimal and maximal values of three numbers: ");
+        System.out.print("Enter first number: ");
+        num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        num2 = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        num3 = input.nextInt();
+
+        if(num1 > num2 && num1 > num3) {
+            greatest = num1;
+        } else if(num2 > num1 && num2 > num3) {
+            greatest = num2;
+        } else {
+            greatest = num3;
+            input.close();
+        }
+        
+        System.out.println("The greatest number is: " + greatest);
+    }
+    
+   
+}
---
+++ array_dpc.java
@@ -0,0 +1,43 @@
+import java.util.Scanner; 
+import java.util.Arrays;
+
+public class array_duplicatecheck {
+          public static void main(String[] args) {
+                    //k = limit 
+                    //n = limit size of array indexes
+                    //x = array size
+
+                    int k, x, arr[], i, a, val;
+                    int result = 0;
+
+                    Scanner input = new Scanner(System.in);
+
+                    String output="No duplicated elements";
+
+                    System.out.println("Enter size of array");
+                    x = input.nextInt();
+                    arr = new int[x];
+
+                    System.out.println("Enter"+x+"array value");
+                    for(i=0; i<x; i++) {
+                              System.out.println("Enter element "+(i));
+                              arr[i]=input.nextInt();
+                    }
+
+                    for (i = 0 ; i < x ; i++) {
+                              for (a = 0; a < x ; a++) {
+                                        if (arr[i] == arr[a]) {
+                                                  result++;
+                                        }
+                              }
+                    }
+                    
+                    if (result < 0) {
+                              output = "Duplicated element";
+                    }
+
+                    String arrayval = Arrays.toString(arr);
+                    System.out.println("\nArray is: "+arrayval);
+                    System.out.println("output: "+output);
+          }
+}
